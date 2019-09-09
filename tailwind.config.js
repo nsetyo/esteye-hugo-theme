c7d1f854790c0509f71c2bc11e9956a981a090aa
Setyo <nsetyo.mail@gmail.com>
2019-09-09 10:56:13 +0700

    Menambah syntax highlight

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 module.exports = {
     theme: {
         fontFamily: {
-            header: 'Livvic, sans-serif',
-            sans  : '"Source Sans Pro", sans-serif'
-
+            header: "Livvic, sans-serif",
+            sans  : "'Source Sans Pro', sans-serif",
+            mono  : "'Source Code Pro', monospace"
         },
     },
     variants: {
