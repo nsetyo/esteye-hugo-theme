72ec2968525261b675f3409a4fe3d9726e14fd81
Setyo <nsetyo.mail@gmail.com>
2019-09-18 21:43:16 +0700

    Menggunakan PurgeCss

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,13 @@
+console.log(process.env.HUGO_ENV);
+
+const purgecss = require('@fullhuman/postcss-purgecss')({
+    // Specify the paths to all of the template files in your project
+    content: [ __dirname + '/layouts/**/*.html' ],
+
+    // Include any special characters you're using in this regular expression
+    defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+});
+
 module.exports = {
     plugins: [
         require('postcss-import')({
@@ -7,6 +17,8 @@ module.exports = {
             ]
         }),
         require('tailwindcss')(__dirname + '/tailwind.config.js'),
-        require('autoprefixer')
+        require('autoprefixer'),
+
+        ...process.env.HUGO_ENV === 'production' ? [purgecss] : []
     ]
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,23 @@
+const { colors } = require('tailwindcss/defaultTheme');
+
 module.exports = {
     theme: {
+        colors: {
+            black : colors.black,
+            gray  : colors.gray,
+            indigo: colors.indigo,
+            white : colors.white,
+        },
         fontFamily: {
             header: "Livvic, sans-serif",
             sans  : "'Source Sans Pro', sans-serif",
             mono  : "'Source Code Pro', monospace"
         },
+        screens: {
+            'sm': '640px',
+            // => @media (min-width: 640px) { ... }
+        }
     },
-    variants: {
-
-    },
-    plugins: [
-
-    ]
+    variants: { },
+    plugins : [ ]
 }
