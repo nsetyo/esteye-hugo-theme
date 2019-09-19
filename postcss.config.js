56327b708cbf327742e123a565fbc7446a539510
Setyo <nsetyo.mail@gmail.com>
2019-09-19 13:57:55 +0700

    Remove purgecss

--- postcss.config.js
+++ postcss.config.js
@@ -1,13 +1,3 @@
-console.log('+++ENV="'+process.env.HUGO_ENV+'"');
-
-const purgecss = require('@fullhuman/postcss-purgecss')({
-    // Specify the paths to all of the template files in your project
-    content: [ __dirname + '/layouts/**/*.html' ],
-
-    // Include any special characters you're using in this regular expression
-    defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
-});
-
 module.exports = {
     plugins: [
         require('postcss-import')({
@@ -18,7 +8,5 @@ module.exports = {
         }),
         require('tailwindcss')(__dirname + '/tailwind.config.js'),
         require('autoprefixer'),
-
-        ...process.env.HUGO_ENV === 'production' ? [purgecss] : []
     ]
 }
