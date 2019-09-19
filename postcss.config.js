b1ee6e8fe869976ed9c18972294b7e6cef47b155
Setyo <nsetyo.mail@gmail.com>
2019-09-19 20:33:12 +0700

    Using PurgeCss with whitelist

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,18 @@
+const purgecss = require('@fullhuman/postcss-purgecss')({
+    // Specify the paths to all of the template files in your project
+    content: [ __dirname + '/layouts/**/*.html' ],
+
+    whitelistPatternsChildren: [
+        /chroma/,
+        /content/,
+        /content--body/,
+        /footnote/
+    ],
+
+    // Include any special characters you're using in this regular expression
+    defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
+});
+
 module.exports = {
     plugins: [
         require('postcss-import')({
@@ -8,5 +23,7 @@ module.exports = {
         }),
         require('tailwindcss')(__dirname + '/tailwind.config.js'),
         require('autoprefixer'),
+
+        ...process.env.HUGO_ENV === 'production' ? [purgecss] : []
     ]
 }
