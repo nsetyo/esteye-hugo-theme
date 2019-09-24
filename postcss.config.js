3b95a830e367964f0b9e6f260b9e0d238d7776fd
Setyo <nsetyo.mail@gmail.com>
2019-09-24 15:52:14 +0700

    Include content to fix whitelisted class

--- postcss.config.js
+++ postcss.config.js
@@ -2,7 +2,7 @@ const purgecss = require('@fullhuman/postcss-purgecss')({
     // Specify the paths to all of the template files in your project
     content: [
         __dirname + '/layouts/**/*.html',
-        __dirname + '/../../public/**/*.html'
+        __dirname + '/../../content/**/*.md'
     ],
 
     whitelistPatternsChildren: [
