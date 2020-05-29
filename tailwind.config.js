13412e62bed967547602261966837031b9885027
Setyo <nsetyo.mail@gmail.com>
2020-05-29 22:29:43 +0700

    Update config as tailwind update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,20 @@
 const { colors } = require('tailwindcss/defaultTheme');
 
 module.exports = {
-    purge: [
-        './layouts/**/*.html',
-    ],
+    purge: {
+        enabled: process.env.HUGO_ENVIRONMENT === 'production',
+        content: ['./layouts/**/*.html'],
+        options: {
+            whitelist: [
+                /tt code kbd samp/,
+                /chroma/,
+                /content/,
+                /content-body/,
+                /footnote/,
+                /highlight/
+            ],
+        }
+    },
     theme: {
         colors: {
             black: colors.black,
