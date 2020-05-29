68c939b4486cfdd9979bea0d2ff3b764d050152c
Setyo <nsetyo.mail@gmail.com>
2020-05-29 19:46:33 +0700

    Bump dependency version

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,28 @@
 const { colors } = require('tailwindcss/defaultTheme');
 
 module.exports = {
+    purge: [
+        './layouts/**/*.html',
+    ],
     theme: {
         colors: {
-            black      : colors.black,
-            gray       : colors.gray,
-            indigo     : colors.indigo,
+            black: colors.black,
+            gray: colors.gray,
+            indigo: colors.indigo,
             transparent: colors.transparent,
-            white      : colors.white,
+            white: colors.white,
         },
         container: {
-            center : true,
+            center: true,
             padding: '1.5rem'
         },
         fontFamily: {
-            sans  : "'Source Sans Pro', sans-serif",
-            mono  : "'Source Code Pro', monospace"
+            sans: "'Source Sans Pro', sans-serif",
+            mono: "'Source Code Pro', monospace"
         },
         screens: {
             'sm': '640px',
             // => @media (min-width: 640px) { ... }
         }
     },
-    variants: { },
-    plugins : [ ]
 }
